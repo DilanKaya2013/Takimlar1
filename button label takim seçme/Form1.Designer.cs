8bc05e2cf19a6a59b393924ff96b3d9b5df2c0f2
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-09-19 15:19:25 +0300

    Add files via upload

---
+++ button label takim seçme/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace button_label_takim_seçme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBesiktas = new System.Windows.Forms.Button();
+            this.btnRealmadrid = new System.Windows.Forms.Button();
+            this.btnBayernmunıh = new System.Windows.Forms.Button();
+            this.btnSamsunspor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTakim = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBesiktas
+            // 
+            this.btnBesiktas.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBesiktas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBesiktas.Location = new System.Drawing.Point(54, 29);
+            this.btnBesiktas.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnBesiktas.Name = "btnBesiktas";
+            this.btnBesiktas.Size = new System.Drawing.Size(348, 63);
+            this.btnBesiktas.TabIndex = 0;
+            this.btnBesiktas.Text = "BEŞİKTAŞ";
+            this.btnBesiktas.UseVisualStyleBackColor = false;
+            this.btnBesiktas.Click += new System.EventHandler(this.btnBesiktas_Click);
+            // 
+            // btnRealmadrid
+            // 
+            this.btnRealmadrid.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRealmadrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRealmadrid.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnRealmadrid.Location = new System.Drawing.Point(54, 139);
+            this.btnRealmadrid.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnRealmadrid.Name = "btnRealmadrid";
+            this.btnRealmadrid.Size = new System.Drawing.Size(348, 63);
+            this.btnRealmadrid.TabIndex = 1;
+            this.btnRealmadrid.Text = "REAL MADRİD";
+            this.btnRealmadrid.UseVisualStyleBackColor = false;
+            this.btnRealmadrid.Click += new System.EventHandler(this.btnRealmadrid_Click);
+            // 
+            // btnBayernmunıh
+            // 
+            this.btnBayernmunıh.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnBayernmunıh.ForeColor = System.Drawing.Color.Maroon;
+            this.btnBayernmunıh.Location = new System.Drawing.Point(54, 250);
+            this.btnBayernmunıh.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnBayernmunıh.Name = "btnBayernmunıh";
+            this.btnBayernmunıh.Size = new System.Drawing.Size(348, 63);
+            this.btnBayernmunıh.TabIndex = 2;
+            this.btnBayernmunıh.Text = "BAYERN MUNIH";
+            this.btnBayernmunıh.UseVisualStyleBackColor = false;
+            this.btnBayernmunıh.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnSamsunspor
+            // 
+            this.btnSamsunspor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSamsunspor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSamsunspor.ForeColor = System.Drawing.Color.Red;
+            this.btnSamsunspor.Location = new System.Drawing.Point(54, 355);
+            this.btnSamsunspor.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSamsunspor.Name = "btnSamsunspor";
+            this.btnSamsunspor.Size = new System.Drawing.Size(348, 63);
+            this.btnSamsunspor.TabIndex = 3;
+            this.btnSamsunspor.Text = "SAMSUNSPOR";
+            this.btnSamsunspor.UseVisualStyleBackColor = false;
+            this.btnSamsunspor.Click += new System.EventHandler(this.btnSamsunspor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 725);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // lblTakim
+            // 
+            this.lblTakim.AutoSize = true;
+            this.lblTakim.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTakim.Location = new System.Drawing.Point(75, 510);
+            this.lblTakim.Name = "lblTakim";
+            this.lblTakim.Size = new System.Drawing.Size(109, 39);
+            this.lblTakim.TabIndex = 5;
+            this.lblTakim.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 865);
+            this.Controls.Add(this.lblTakim);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSamsunspor);
+            this.Controls.Add(this.btnBayernmunıh);
+            this.Controls.Add(this.btnRealmadrid);
+            this.Controls.Add(this.btnBesiktas);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBesiktas;
+        private System.Windows.Forms.Button btnRealmadrid;
+        private System.Windows.Forms.Button btnBayernmunıh;
+        private System.Windows.Forms.Button btnSamsunspor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTakim;
+    }
+}
+
---
+++ button label takim seçme/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button_label_takim_seçme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBesiktas_Click(object sender, EventArgs e)
+        {
+            lblTakim.Text = "BEŞİKTAŞ";
+            lblTakim.BackColor = Color.White;
+            lblTakim.ForeColor = Color.Black;
+        }
+
+        private void btnRealmadrid_Click(object sender, EventArgs e)
+        {
+            lblTakim.Text = "REAL MADRİD";
+
+
+            lblTakim.BackColor = Color.Black;
+
+            lblTakim.ForeColor = Color.White;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblTakim.Text = "BAYERN MUNİH";
+
+            lblTakim.BackColor = Color.Blue;
+
+            lblTakim.ForeColor = Color.Purple;
+
+
+        }
+
+        private void btnSamsunspor_Click(object sender, EventArgs e)
+        {
+            lblTakim.Text = "SAMSUNSPOR";
+
+            lblTakim.ForeColor = Color.Red;
+
+            lblTakim.BackColor = Color.White;
+
+        }
+    }
+}
